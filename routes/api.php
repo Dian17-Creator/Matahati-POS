6f1db6f811ce2d42666e367dae25547854896618
aditiyapratama <aditiyapratama2014@gmail.com>
2026-09-10 10:48:07 +0700

    Feat : Add Api Voucher

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,14 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Master\CategoryController;
 use App\Http\Controllers\Master\ProductController;
 use App\Http\Controllers\Master\CustomerController;
+use App\Http\Controllers\Master\VoucherController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/categories', [CategoryController::class, 'apiIndex']);
 Route::get('/products', [ProductController::class, 'apiIndex']);
 Route::get('/customers', [CustomerController::class, 'apiIndex']);
+Route::get('/vouchers', [VoucherController::class, 'apiIndex']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
