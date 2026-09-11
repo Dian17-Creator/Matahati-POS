fbd3b791e678ec8f543b943660341050f25a8720
aditiyapratama <aditiyapratama2014@gmail.com>
2026-09-11 11:05:25 +0700

    Update : api

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Master\ProductController;
 use App\Http\Controllers\Master\CustomerController;
 use App\Http\Controllers\Master\VoucherController;
 use App\Http\Controllers\Master\PaymentController;
+use App\Models\MposSalesH;
 
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -21,3 +22,21 @@ Route::get('/payment-methods', [PaymentController::class, 'apiIndex']);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//API ORDER-TYPE
+Route::get('/pos/order-types', function () {
+    return response()->json([
+        'success' => true,
+        'data' => collect(MposSalesH::ORDER_TYPES)->map(function ($type) {
+            return [
+                'value' => $type,
+                'label' => match ($type) {
+                    MposSalesH::ORDER_TYPE_DINE_IN => 'DINE-IN',
+                    MposSalesH::ORDER_TYPE_TAKE_AWAY => 'TAKE-AWAY',
+                    MposSalesH::ORDER_TYPE_ONLINE => 'ONLINE',
+                    default => $type,
+                },
+            ];
+        })->values(),
+    ]);
+});
