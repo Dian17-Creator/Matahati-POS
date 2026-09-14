1ae291b4d3d589c1a5c0742ca00957bc8d345766
aditiyapratama <aditiyapratama2014@gmail.com>
2026-09-14 10:50:53 +0700

    Fix : Bug time utc to wib

--- app/Models/MposSalesH.php
+++ app/Models/MposSalesH.php
@@ -100,6 +100,17 @@ class MposSalesH extends Model
         ];
     }
 
+    /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(\DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+
     public function customer()
     {
         return $this->belongsTo(
